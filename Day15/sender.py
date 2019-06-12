52cca1f8b67697fa0a7f934c841f524e363fc58a
Li.Shangzhi <13680348517@163.com>
2019-06-12 14:42:25 +0800

    :sparkles: Python 微信网页端登录与自动回复

--- Day15/sender.py
+++ Day15/sender.py
@@ -1,3 +1,8 @@
+""" 欢迎关注 "码农架构" 微信公众号，热爱开源，拥抱开源。一个IT民工的技术之路经验分享。
+    - 问题咨询 / 建议
+    1.关注微信公众号 "码农架构" 后私信
+    2.可发送邮件: li.shangzhi@aliyun.com
+"""
 # coding: utf-8
 from __future__ import unicode_literals
 from threading import Timer
